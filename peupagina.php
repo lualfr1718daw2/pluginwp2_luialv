28dabee71ed4f402dff25b231b1dbb69be8e98a5
root <root@daw2m08.fjeclot.net>
2018-04-25 20:20:13 +0200

    commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Luis Alonso Alva Franco
+Author URI: https://github.com/lualfr1718daw2
+License: GNU GPLv3 
+*/ 
+?>
